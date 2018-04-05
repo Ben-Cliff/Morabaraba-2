d05ab7a2703e72b9463fa278055848b2a39b8fd7
Ernest Loveland <edg3@users.noreply.github.com>
2018-04-05 07:26:45 +0200

    debug mode complete, team notified

--- src/play_me/play_me/_PlaceCow.py
+++ src/play_me/play_me/_PlaceCow.py
@@ -1,6 +1,5 @@
-def place(self):
-    game_board = self.game_board
+def place(self, game_board):
     # Place a cow
     print('you didnt place one, however this is just to show how we swap functions')
-    print("- I return game_board as: " + str(game_board))
+    print(" ~~~ place supposedly return game_board as: " + str(game_board))
     game_board
--- src/play_me/play_me/morabarabarabarabarabarabarabarabaraba.py
+++ src/play_me/play_me/morabarabarabarabarabarabarabarabaraba.py
@@ -9,16 +9,18 @@ class game(object):
     # def board_replace(whre,wht):
     #     game_board[where] = wht
 
-    def draw_board(self):
+    def draw_board(self, game_board):
         # Assigned: Ernest
         # draw the board
-        print(" ~~~ " + str(self.game_board))
-        _DrawBoard.draw_board(self.game_board)
+        print(" ~~~ " + str(game_board))
+        _DrawBoard.draw_board(game_board)
 
-    def place(self):
+    def place(self, game_board):
         # Assigned: 
         # get input and place
-        self.game_board = _PlaceCow.place(self)
+        game_board = _PlaceCow.place(self, game_board)
+        print(" -=- I got this from place: " + str(game_board))
+        game_board
 
     def move(self, can_it_fly):
         # Assigned: 
@@ -35,19 +37,20 @@ class game(object):
         # Check the mills for this player
         self.game_board = _IsMillFor.is_there_a_mill_for(self.game_board)
 
-    def main(self):
+    def main(self, game_board):
         # Assigned: 
         # Main Life of Game : interaction
-        self.draw_board(self)
+        print(" ~~~ I begin with self.game_board: " + str(game_board))
+        self.draw_board(self, game_board)
         i_got = input("Tell me to do something? ")
         # use input to choose what the game plays
-        self.game_board = self.place(self)
-        print("- 4. game board to: " + str(self.game_board))
-
-    def define_game_board(self):
-        self.game_board = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        banana = self.place(self, game_board)
+        print(" ~~~ Bananana shows us it didnt return the game_board? : " + str(banana))
+        game_board = banana
+        print(" ~~~ I set game board to: " + str(game_board))
+        game_board # surely this returns the new board
 
 g = game
-g.define_game_board(g)
+game_board = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 while True:
-    g.main(g)
+    game_board = g.main(g, game_board)
