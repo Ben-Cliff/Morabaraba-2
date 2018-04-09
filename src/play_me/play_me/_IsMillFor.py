01b9530e868b4c274c64e2b9f2e112eb99e115ee
g15l2589 <loveland.ernest@gmail.com>
2018-04-09 16:35:53 +0200

    reminders

--- src/play_me/play_me/_IsMillFor.py
+++ src/play_me/play_me/_IsMillFor.py
@@ -1,3 +1,6 @@
+import _DataStructures
+
 def is_there_a_mill_for(game_board, i_got):
     # Check the mills for this player
+    _DataStructures.mills
     return game_board
--- src/play_me/play_me/_MoveCow.py
+++ src/play_me/play_me/_MoveCow.py
@@ -1,3 +1,4 @@
 def move(game_board, can_it_fly, i_got):
+    #can_it_fly  boolean check if player has 3 cows
     # pick a spot then move it
     return game_board
