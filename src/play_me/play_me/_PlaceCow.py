de55848ed390aec091461ccf6cc25dd3d3a383a0
Ernest Loveland <edg3@users.noreply.github.com>
2018-04-09 18:44:26 +0200

    file explanation

--- src/play_me/play_me/_PlaceCow.py
+++ src/play_me/play_me/_PlaceCow.py
@@ -1,16 +1,24 @@
+# When we want reference to the other files this is what we add
+#   -> reminder we would us the file name followed by the value
+#      -> e.g. _PlayerData.PlayerType.NOT     <- correct
+#              PlayerType.NOT                 <- incorrect
 import _DataStructures
 import _PlayerData
 
 import time
 
 # Place a cow
+#  -> i_got is the string input we accepted in main and received in this external function call
+#  -> self is the instance of Game we are using/playing with
+#    -> you will see that it is where we can reference game_board, either player, etc
 def place(self, i_got):
     # First check if the choice was correct
+	#  -> this gets the index in the array for the board
+	#    -> note: because we double check the input before we know i_git is in the "allPositions", we only need the index
     boardPos = _DataStructures.allPositions.index(i_got)
     then_got = boardPos
     
     # Error handling - check the move to make sure we can use it (empty and an actual position)
-
     if self.game_board[boardPos]!=_PlayerData.PlayerType.NOT:           #occupiedcheck
         while self.game_board[then_got]!=_PlayerData.PlayerType.NOT:    #while user input incorrct, ask again
             then_got = input(" ~ Oops, the tile needs to be empty please pick again:\n\t\t")
@@ -19,7 +27,11 @@ def place(self, i_got):
     self.game_board[then_got] = self.whosTurn                           #edit board
 
     # Let the player know with a certain amount of time
+	#   -> just so it doesnt instantly show the next turn
     print('\nNeat, you placed one!\n')
     time.sleep(2)
     
+	# finally like I shared before we currently return the updated board
+	#   -> we will remove this later unless we find a need to keep it
+	#   -> this is mostly for debugging so you can call "print" for any state of board, etc
     return self.game_board
