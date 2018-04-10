9be245397afda43669769f2cd128055349e7b18b
Tiaan-H <tiharmse@gmail.com>
2018-04-10 22:37:22 +0200

    Completed MoveCow

--- src/play_me/play_me/_MoveCow.py
+++ src/play_me/play_me/_MoveCow.py
@@ -19,6 +19,15 @@ def move(game_board, can_it_fly, playertype, self):
     else:
         #move
         # -> we can only pick positions empty that are NEXT TO the position we choose
-        frm = pick_valid_spot(game_board, 2, playertype, _PlayerData.whatName(playertype) + " player's turn to move. Enter a coordinate to move a cow from", _PlayerData.whatName(playertype) + " player, you do not have a cow in that position. Please enter another co ordinate ")
-        to = pick_valid_spot(game_board, 3, playertype, "Now enter a coordinate to move the cow to. (Must be empty and must be one unit away)", _PlayerData.whatName(playertype) + " player, you do not have a cow in that position. Please enter another co ordinate ")
+        unitbool = False
+        while unitbool == False:
+            frm = pick_valid_spot(game_board, 2, playertype, _PlayerData.whatName(playertype) + " player's turn to move. Enter a coordinate to move a cow from", _PlayerData.whatName(playertype) + " player, you do not have a cow in that position. Please enter another co ordinate ")
+            to = pick_valid_spot(game_board, 3, playertype, "Now enter a coordinate to move the cow to. (Must be empty and must be one unit away)", _PlayerData.whatName(playertype) + " player, you do not have a cow in that position. Please enter another co ordinate ")
+            if _DataStructures.one_away[_DataStructures.allPositions.index(frm)].count(_DataStructures.allPositions.index(to)) > 0 :        #Tests that player is moving by one unit
+                unitbool = True
+        game_board[frm] = _PlayerData.PlayerType.NOT
+        game_board[to] = playertype
+        is_there_mill = _IsMillFor.is_there_a_mill_for(playertype, game_board, _DataStructures.allPositions[to])
+        if is_there_mill:
+            self.shoot(playertype, game_board)
     return game_board
