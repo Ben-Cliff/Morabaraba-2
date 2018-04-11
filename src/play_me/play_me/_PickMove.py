961947a27a1f54d54376aca12a708d299bcd7e90
Ernest Loveland <edg3@users.noreply.github.com>
2018-04-11 07:49:26 +0200

    updated spacing

--- src/play_me/play_me/_PickMove.py
+++ src/play_me/play_me/_PickMove.py
@@ -1,6 +1,7 @@
 import _DataStructures
 import _PlayerData
 import _IsMillFor
+
 def pick_valid_spot(game_board, option, player, msg, err):      #Options: 1-Empty 2-Player's cow 3-Enemy Player's Cow
     
     if option==1:   #Expecting Empty Spot
@@ -8,22 +9,23 @@ def pick_valid_spot(game_board, option, player, msg, err):      #Options: 1-Empt
         for x in range(24):
             if _DataStructures.get_player_icon[game_board[x]] == " ":
                 avail.append(_DataStructures.allPositions[x])
+
         then_got = input("\t" + msg + "\n\t\t")
+
         while (not then_got in avail):
-            
             then_got  = input(" ~ " + err);
         
         return _DataStructures.allPositions.index(then_got)
-
 
     if option==2:   #Expecting Player's cow in spot
         avail = []
         for x in range(24):
             if game_board[x]==player:
                 avail.append(_DataStructures.allPositions[x])
+
         then_got = input("\t" + msg + "\n\t\t")
+
         while (not then_got in avail):
-            
             then_got  = input(" ~ " + err);
         
         return _DataStructures.allPositions.index(then_got)
@@ -38,12 +40,12 @@ def pick_valid_spot(game_board, option, player, msg, err):      #Options: 1-Empt
             otherplayer = _DataStructures.RED
         
         for x in range(24):
-
             if (game_board[x]==player) and (_IsMillFor.is_there_a_mill_for(game_board[x],game_board, _DataStructures.allPositions[x]) == False):
                 avail.append(_DataStructures.allPositions[x])
+
         then_got = input("\t" + msg + "\n\t\t")
+
         while (not then_got in avail):
-            
             then_got  = input(" ~ " + err);
         
         return _DataStructures.allPositions.index(then_got)
